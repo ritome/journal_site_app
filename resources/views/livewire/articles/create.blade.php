a24cb570377cb98c83e394b4b67d733032c4897e
ritome <ritome0109@gmail.com>
2025-09-01 22:11:30 +0900

    バリテーションとエラーメッセージの日本語化完了

--- resources/views/livewire/articles/create.blade.php
+++ resources/views/livewire/articles/create.blade.php
@@ -1,12 +1,19 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Article;
 
 state(['title', 'body']);
 
+// バリデーションルール
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:5000',
+]);
+
 // メモを保存する関数
 $store = function () {
+    $this->validate();
     // フォームからの入力値をうデータベースへ保存
     Article::create([
         'title' => $this->title,
@@ -19,18 +26,26 @@ $store = function () {
 ?>
 
 <div>
-    
+
     <h1>新規論文投稿</h1>
 
     <!-- wire:submit="store"でフォーム送信時にstore関数を呼び出し -->
     <form wire:submit="store">
         <p>
-            <label for="title">論文タイトル</label><br>
+            <label for="title">論文タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
--- resources/views/livewire/articles/edit.blade.php
+++ resources/views/livewire/articles/edit.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Article;
 
 // フォームの状態を管理
@@ -13,7 +13,14 @@ mount(function (Article $article) {
     $this->body = $article->body;
 });
 
+// バリデーションルール
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:5000',
+]);
+
 $update = function () {
+    $this->validate();
     $this->article->update($this->all());
     return redirect()->route('articles.show', $this->article);
 };
@@ -26,12 +33,20 @@ $update = function () {
     <!-- wire:submit="update"でフォーム送信時にupdate関数を呼び出し -->
     <form wire:submit="update">
         <p>
-            <label for="title">論文タイトル</label><br>
+            <label for="title">論文タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
