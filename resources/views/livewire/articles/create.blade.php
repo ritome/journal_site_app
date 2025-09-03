bd48b4e552483e2649368caef529c84d7efa6776
ritome <ritome0109@gmail.com>
2025-09-03 17:06:09 +0900

    入力履歴機能追加

--- resources/views/livewire/articles/create.blade.php
+++ resources/views/livewire/articles/create.blade.php
@@ -38,7 +38,7 @@ $store = function () {
             @enderror
             <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
-            <input type="text" wire:model="title" id="title">
+            <input type="text" wire:model="title" id="title" autocomplete="on">
         </p>
         <p>
             <label for="body">本文</label>
@@ -47,7 +47,7 @@ $store = function () {
             @enderror
             <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
-            <textarea wire:model="body" id="body"></textarea>
+            <textarea wire:model="body" id="body" autocomplete="on"></textarea>
         </p>
 
         <button type="submit">投稿</button>
--- resources/views/livewire/articles/edit.blade.php
+++ resources/views/livewire/articles/edit.blade.php
@@ -39,7 +39,7 @@ $update = function () {
             @enderror
             <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
-            <input type="text" wire:model="title" id="title">
+            <input type="text" wire:model="title" id="title" autocomplete="on">
         </p>
         <p>
             <label for="body">本文</label>
@@ -48,7 +48,7 @@ $update = function () {
             @enderror
             <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
-            <textarea wire:model="body" id="body"></textarea>
+            <textarea wire:model="body" id="body" autocomplete="on"></textarea>
         </p>
 
         <button type="submit">更新</button>
